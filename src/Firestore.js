66fba24bc9c114549326b8b97e4e48f6ab4f9c25
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-03-04 11:20:14 -0500

    npm install firebase

---
+++ src/Firestore.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAYWbt8vq9P4zGU5PknVFQ8-Twm7W_ILNQ",
+    authDomain: "firethrone-ce9cd.firebaseapp.com",
+    databaseURL: "https://firethrone-ce9cd.firebaseio.com",
+    projectId: "firethrone-ce9cd",
+    storageBucket: "firethrone-ce9cd.appspot.com",
+    messagingSenderId: "385660047741"
+  };
+firebase.initializeApp(config);
+
+export default firestore
