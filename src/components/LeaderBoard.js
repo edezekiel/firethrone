d8750cc1e3a140f84828649599d55154f854e166
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-03-11 17:22:22 -0400

    pulled player scores down from firebase, but sort is not yet implemented

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react'
-
+import firebase from 'firebase'
 import { Container, Header, List, Segment } from 'semantic-ui-react'
 
 class LeaderBoard extends Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount() {
+    this.getPlayers()
+  }
+
+  getPlayers = () => {
+    const db = firebase.firestore();
+    db.collection('players').get()
+    .then(res => res.docs.forEach((el) => {
+        this.setState({ players: [...this.state.players, el.data()]})
+    }))
+  }
 
   render() {
+    console.log(this.state)
     return(
       <Container textAlign="center" style={{"padding-top": "10vh", "width": "50%"}}>
         <Segment inverted>
-          <Header inverted as="h1">Top Scores</Header>
-          <List animated inverted divided relaxed size="massive" verticalAlign="middle">
-            <List.Item>
-              <List.Content floated="left">Ed</List.Content>
-              <List.Content floated='right'>
-                Score
-              </List.Content>
-            </List.Item>
 
-            <List.Item>
-              <List.Content floated="left">Bob</List.Content>
-              <List.Content floated='right'>
-                Score
-              </List.Content>
-            </List.Item>
+          {this.state.players.length ?
+          <div>
+            <Header inverted as="h1">Top Scores:</Header>
+            {this.state.players.map(player =>
+              <List animated inverted divided relaxed size="massive" verticalAlign="middle">
+                <List.Item>
+                  <List.Content floated="left">{player.player}</List.Content>
+                  <List.Content floated='right'>
+                    {player.score}
+                  </List.Content>
+                </List.Item>
+              </List>
+            )}
+          </div>
 
-            <List.Item>
-              <List.Content floated="left">Barry</List.Content>
-              <List.Content floated='right'>
-                Score
-              </List.Content>
-            </List.Item>
-          </List>
+          : null
+          }
           </Segment>
       </Container>
 
